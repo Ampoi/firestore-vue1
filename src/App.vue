05bbbf6d7cfcb436eeb5df63cd08bb4b91fa2f16
S.kuronosuke <71966872+shibata-kuronosuke@users.noreply.github.com>
2022-01-29 07:02:08 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+import { initializeApp } from "firebase/app";
+import { getAnalytics,logEvent } from "firebase/analytics";
+// 以下firebase
+const firebaseConfig = {
+    apiKey: "***",
+    authDomain: "***",
+    projectId: "***",
+    storageBucket: "***",
+    messagingSenderId: "***",
+    appId: "***",
+    measurementId: "***"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+logEvent(analytics, 'notification_received');
+
+export default {
+  name: "app"
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <button v-on:click="upload">upload!!!!</button>
+  </div>
+</template>
+
+<script>
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+async function hoge() {
+  const db = getFirestore();
+
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+export default {
+  name: 'Home',
+  methods: {
+    upload (){
+      hoge()
+    }
+  }
+}
+</script>
